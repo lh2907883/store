2e415e623428c25ccd679c8eed8b3b1e15b8260b
hao.li3 <hao.li3@renren-inc.com>
2023-11-25 11:25:20 +0800

    update

--- homework/index.js
+++ homework/index.js
@@ -11,13 +11,13 @@ $(function(){
         // 11-2 18-9
         for(var i = 11; i <= 18; i++){
             for (var j = i - 9; j<= 9; j++){
-                topic.push(`${i} - ${j} =`);
+                topic.push({sum: i, a: j});
             }
         }
         // 10(20)-1 10(20)-9
         for(var i = 1; i <= 9; i++){
             var x = generateRandomNumber(0, 1) === 0 ? 10 : 20;
-            topic.push(`${x} - ${i} =`);
+            topic.push({sum: x, a: i});
         }
         // // 100-1 100-9 100-11 100-99
         // for(var i = 1; i <= 9; i++){
@@ -28,8 +28,14 @@ $(function(){
     $('.btn-gen').on('click', function(){
         var content = '';
         for(var i = 0; i < 50; i++){
-            var index = generateRandomNumber(0, topic.length - 1)
-            content += `<div class="cell">${topic[index]}</div>`;
+            var index = generateRandomNumber(0, topic.length - 1);
+            var flag = generateRandomNumber(0, 1) === 0;
+            var obj = topic[index];
+            content += `<div class="cell">
+                <div>${obj.sum}</div>
+                <div>/&nbsp;&nbsp;&nbsp;&nbsp;\\</div>
+                <div>${flag ? '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + obj.a : obj.a + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'}</div>
+            </div>`;
         }
         $('#table').html(content);
     });
